2733c7257c5b002c7642aa9571ffae2af856c83c
heojeop <20191246@vision.hoseo.edu>
2022-12-05 03:36:37 +0900

    Player Movement 수정
    카메라 이동 수정

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -5,36 +5,31 @@ using UnityEngine;
 public class PlayerCtrl : MonoBehaviour
 {
 
-    private float h = 0f;
-    private float v = 0f;
-    private Transform tr;
-    public float speed = 10.0f;
-    public float rotSpeed = 100.0f;
-    private Animator animator;
-    // Start is called before the first frame update
+    [SerializeField]
+    private float walkSpeed;
+
+    private Rigidbody myRigid;
+
     void Start()
     {
-        tr = GetComponent<Transform>();
-        animator = GetComponent<Animator>();
+        myRigid = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        h = Input.GetAxis("Horizontal");
-        v = Input.GetAxis("Vertical");
+        Move();
+    }
 
-        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
-        tr.Translate(moveDir.normalized * Time.deltaTime * speed);
-        tr.Rotate(Vector3.up * Time.deltaTime * rotSpeed * Input.GetAxis("Mouse X"));
+    private void Move()
+    {
+        float _moveDirX = Input.GetAxisRaw("Horizontal");
+        float _moveDirZ = Input.GetAxisRaw("Vertical");
 
-        if(moveDir.magnitude > 0)
-        {
-            animator.SetFloat("Speed", 1.0f);
-        }
-        else
-        {
-            animator.SetFloat("Speed", 0.0f);
-        }
+        Vector3 _moveHoizontal = transform.right * _moveDirX;
+        Vector3 _moveVertial = transform.forward * _moveDirZ;
+
+        Vector3 _velocity = (_moveHoizontal + _moveVertial).normalized * walkSpeed;
+
+        myRigid.MovePosition(transform.position + _velocity * Time.deltaTime);
     }
 }
