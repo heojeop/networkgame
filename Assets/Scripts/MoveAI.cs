697d50e91cab19a986bd79917087ecae54344397
TheLeafeon <zkjx12@gmail.com>
2022-12-12 10:45:20 +0900

    Changing MoveAI
    움직이는 AI 수정중

--- Assets/Scripts/MoveAI.cs
+++ Assets/Scripts/MoveAI.cs
@@ -1,17 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class MoveAI : MonoBehaviour
 {
 
+
     public Rigidbody AiCharacter;
     float randomMovingTime;
+
+
+    private PhtonView pv;
+    private Transform tr;
+    public float speed = 10.0f;
+    public float rotSpeed = 100.0f;
+    private Animation animator;
 
     // Start is called before the first frame update
     void Start()
     {
+        tr = GetComponent<Transform>();
+        animator = GetComponent<animator>();
+        pv = GetComponent<PhotonView>();
+
         StartCoroutine(MoveObject());
+
 
 
     }
@@ -24,6 +38,12 @@ public class MoveAI : MonoBehaviour
         {
             float dirX = Random.Range(-6f, 6f);
             float dirZ = Random.Range(-6f, 6f);
+
+            Vector3 moveDir = (Vector3.forward * dirX) + (Vector.rightt * dirZ);
+
+            tr.Translate(moveDir.normailized * randomMovingTime.deltaTime * speed);
+            //tr.Rotate(Vector3.up  * Time.deltaTime * rotSpeed * )
+
 
             randomMovingTime = Random.Range(0.1f, 1f);
 
