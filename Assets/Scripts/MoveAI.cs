f7feaa10bcd2b228f2c3e25ba53bc2f44739848a
TheLeafeon <zkjx12@gmail.com>
2022-12-13 05:15:03 +0900

    Changing MoveAI2
    움직이는 AI 수정중2 움직이는 방법 변경 할 필요 있음 부자연스러움

--- Assets/Scripts/MoveAI.cs
+++ Assets/Scripts/MoveAI.cs
@@ -1,61 +1,101 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using Photon.Pun;
 
 public class MoveAI : MonoBehaviour
 {
+    public enum AICharacterState {idle, trace, die };
+    public AICharacterState AIState = AICharacterState.idle;
 
+    private Transform AICharacterTr;
+    public Rigidbody AICharacter;
+    private NavMeshAgent nvAgent;
+    private Animator animator;
 
-    public Rigidbody AiCharacter;
     float randomMovingTime;
-
-
-    private PhtonView pv;
-    private Transform tr;
     public float speed = 10.0f;
-    public float rotSpeed = 100.0f;
-    private Animation animator;
+    public bool isDie = false;
+    public float idleDist = 2.0f;
+    public float rotateSpeed = 10.0f;
+    
+    float rotH, rotV;
 
     // Start is called before the first frame update
     void Start()
     {
-        tr = GetComponent<Transform>();
-        animator = GetComponent<animator>();
-        pv = GetComponent<PhotonView>();
-
-        StartCoroutine(MoveObject());
-
+        AICharacterTr = GetComponent<Transform>();
+        nvAgent = GetComponent<NavMeshAgent>();
+        //AICharacter = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
 
 
-    }
-
-    IEnumerator MoveObject()
-    {
-        AiCharacter = GetComponent<Rigidbody>();
-
-        while (true)
         {
-            float dirX = Random.Range(-6f, 6f);
-            float dirZ = Random.Range(-6f, 6f);
-
-            Vector3 moveDir = (Vector3.forward * dirX) + (Vector.rightt * dirZ);
-
-            tr.Translate(moveDir.normailized * randomMovingTime.deltaTime * speed);
-            //tr.Rotate(Vector3.up  * Time.deltaTime * rotSpeed * )
-
-
-            randomMovingTime = Random.Range(0.1f, 1f);
-
-            yield return new WaitForSeconds(randomMovingTime);
-            AiCharacter.velocity = new Vector3(dirX, 0, dirZ);
+            StartCoroutine(this.CheckAICharacterState());
         }
 
-
-
     }
 
-    // Update is called once per frame
+    IEnumerator CheckAICharacterState()
+    {
+        while(isDie==false)
+        {
+            yield return new WaitForSeconds(0.05f);
+
+            float dirX = Random.Range(-2f, 2f);
+             float dirZ = Random.Range(-2f, 2f);
+             bool dist = true;
+
+            rotH = dirX;
+            rotV = dirZ;
+
+
+            if (dirX <2 && dirX> -2)
+            {
+                if (dirZ < 2 && dirZ > -2)
+                {
+                    dist = true;
+                }
+                else
+                {
+                    dist = false;
+                }
+            }
+            else
+            {
+                dist = false;
+            }
+            
+
+            if (AIState == AICharacterState.die)
+            {
+                ;
+            }
+            //dist 가 true면 가만히
+            if (dist == true)
+            {
+                AIState = AICharacterState.idle;
+                Debug.Log("CheckAICharacterState(): idle! " + dirX + dirZ);
+            }
+            else
+            {
+                AIState = AICharacterState.trace;
+                Debug.Log("CheckAICharacterState(): trace! " + dirX + dirZ);
+            }
+
+            Vector3 rotDir = new Vector3(rotH, 0, rotV);
+
+            if (!(rotH == 0 && rotV == 0))
+            {
+                AICharacter.position += rotDir * speed * Time.deltaTime;
+
+                AICharacter.rotation = Quaternion.Lerp(AICharacter.rotation, Quaternion.LookRotation(rotDir), Time.deltaTime * rotateSpeed);
+            }
+
+        }
+    }
+
     void Update()
     {
 
